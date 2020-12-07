b266eecc8785a48fd2561ddfb325fec4c47be315
Szabolcs01 <oszabi000@gmail.com>
2020-12-07 10:23:52 +0100

    Kapcsolat létre hozzása

--- HarcosokApplication/Form1.cs
+++ HarcosokApplication/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace HarcosokApplication
 {
     public partial class Form1 : Form
     {
+        MySqlConnection conn = null;
+        MySqlCommand sql = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder harcosadatbazis = new MySqlConnectionStringBuilder();
+            harcosadatbazis.Server = "localhost";
+            harcosadatbazis.UserID = "root";
+            harcosadatbazis.Password = "";
+            harcosadatbazis.Database = "cs_harcosok";
+            harcosadatbazis.CharacterSet = "utf8";
+            conn = new MySqlConnection(harcosadatbazis.ToString());
+            try
+            {
+                conn.Open();
+                sql = conn.CreateCommand();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Adatkapcsolati hiba");
+                return;
+            }
+
+            conn.Close();
         }
     }
 }
